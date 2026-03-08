8217ae17261e16c2b6d9b5678725b7a8c6af297e
Muskan-Syed <syedmuskan0345@gmail.com>
2026-03-08 20:15:14 +0530

    Update file IDs for downloading artifacts

--- app/utils/download_data.py
+++ app/utils/download_data.py
@@ -6,20 +6,23 @@ PROJECT_DIR = Path(__file__).resolve().parent.parent.parent
 ARTIFACT_DIR = PROJECT_DIR / "data/artifacts"
 PROCESSED_DIR = PROJECT_DIR / "data/processed"
 
+
 def download_artifacts():
     ARTIFACT_DIR.mkdir(parents=True, exist_ok=True)
 
     files = {
-        "faiss_index.bin": "1xfekt8E5ZJLN8GyElQFQvbTFx9VmHg8h",
-        "document_embeddings.npy": "17BK0bVAlvw4GNGBzcldQNB9_HnAMnQu4"
+        "faiss_index.bin": "1rM0fABxhsj_dR5C3kHH3z1j7w_58sjQZ",
+        "document_embeddings.npy": "1ZjzS_UUeILL95c53KQLLf6ChbWgBWxG5",
     }
 
     for filename, file_id in files.items():
         output = ARTIFACT_DIR / filename
+
         if not output.exists():
             url = f"https://drive.google.com/uc?id={file_id}"
             print(f"Downloading {filename}...")
             gdown.download(url, str(output), quiet=False)
+
 
 def download_processed_dataset():
     PROCESSED_DIR.mkdir(parents=True, exist_ok=True)
